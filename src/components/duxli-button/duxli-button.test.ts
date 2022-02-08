8e95082436f917d6a45f1ca9590547e0933f642d
Michelle Miller <chellimiller@users.noreply.github.com>
2022-02-08 16:45:55 -0700

    Initialize Stencil

---
+++ src/components/duxli-button/duxli-button.test.ts
@@ -0,0 +1,36 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { MyComponent } from './duxli-button';
+
+describe('duxli-button', () => {
+  it('renders', async () => {
+    const { root } = await newSpecPage({
+      components: [MyComponent],
+      html: '<duxli-button></duxli-button>',
+    });
+    expect(root).toEqualHtml(`
+      <duxli-button>
+        <mock:shadow-root>
+          <div>
+            Hello, World! I'm
+          </div>
+        </mock:shadow-root>
+      </duxli-button>
+    `);
+  });
+
+  it('renders with values', async () => {
+    const { root } = await newSpecPage({
+      components: [MyComponent],
+      html: `<duxli-button first="Stencil" last="'Don't call me a framework' JS"></duxli-button>`,
+    });
+    expect(root).toEqualHtml(`
+      <duxli-button first="Stencil" last="'Don't call me a framework' JS">
+        <mock:shadow-root>
+          <div>
+            Hello, World! I'm Stencil 'Don't call me a framework' JS
+          </div>
+        </mock:shadow-root>
+      </duxli-button>
+    `);
+  });
+});
